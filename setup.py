9685310f7a5df9dc7e4c8476f59fd382c76e8932
Ori <45724082+therealOri@users.noreply.github.com>
2022-02-27 13:59:38 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,17 +6,11 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setuptools.setup(
     name="oCrypt0r",
     packages=["ocryptor"],
-    version="1.0.1",
+    version="1.0.2",
     author="therealOri",
     license="GPL-3.0",
     install_requires=[
-        "colorama==0.4.4",
-        "halo==0.0.31",
-        "log-symbols==0.0.14",
         "pycryptodome==3.14.1",
-        "six==1.16.0",
-        "spinners==0.0.24",
-        "termcolor==1.1.0",
     ],
     author_email="therealOri@duck.com",
     description="A minimalistic, simple AES encryption library written in python3.",
