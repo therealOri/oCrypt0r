5aee3dbfbbbb363e002fd514b1c3609a1d15fe46
Ori <45724082+therealOri@users.noreply.github.com>
2022-02-27 19:20:39 -0700

    Directory Encryption!
    Added:
    - A new way to encrypt a specified directory. It will encrypt all of the files in that directory and sub-directories. So if there are folders in folders..it'll do them too.
    
    Changed:
    - How error handling is done, functions will now return "False" if any errors happen and "True" if successful. Instead of "None"

--- ocryptor/main.py
+++ ocryptor/main.py
@@ -54,50 +54,87 @@ class oCrypt:
             self.clear()
             return d_cipher_data.decode()
         else:
-            return None
+            return False
 
 
     def file_encrypt(self, file, salt):
         BUF_SIZE = 65536
-        
-        key = PBKDF2(self.hash_key, salt, dkLen=32)
-        rb = get_random_bytes(AES.block_size)
-        cipher = AES.new(key, AES.MODE_CBC, rb)
-
-        with open(file, 'r+b') as f:
-            while True:
-                data = f.read(BUF_SIZE)
-                if not data:
-                    break
-                cipher_data = base64.b64encode(rb + cipher.encrypt(pad(data, AES.block_size)))
-        with open(file, 'wb') as f2:
-            f2.write(cipher_data)
-            os.rename(file, f'{file}.oCrypted')
-        return
+        isFile = os.path.isfile(file)
+        if isFile == True:
+            key = PBKDF2(self.hash_key, salt, dkLen=32)
+            rb = get_random_bytes(AES.block_size)
+            cipher = AES.new(key, AES.MODE_CBC, rb)
+            try:
+                with open(file, 'rb') as f:
+                    while True:
+                        data = f.read(BUF_SIZE)
+                        if not data:
+                            break
+                        cipher_data = base64.b64encode(rb + cipher.encrypt(pad(data, AES.block_size)))
+                with open(file, 'wb') as f2:
+                    f2.write(cipher_data)
+                    os.rename(file, f'{file}.oCrypted')
+            except Exception as e:
+                return False
+            return True
+        else:
+            return False
 
     
     def file_decrypt(self, file, salt):
-        FLAG2 = True
-        with open(file, 'rb') as f:
-            string = f.read()
-            b64d = base64.b64decode(string)
-
+        isFile = os.path.isfile(file)
+        if isFile == True:
             try:
-                key = PBKDF2(self.hash_key, salt, dkLen=32)
-                cipher = AES.new(key, AES.MODE_CBC, b64d[:AES.block_size])
-                d_cipher_data = unpad(cipher.decrypt(b64d[AES.block_size:]), AES.block_size)
+                with open(file, 'rb') as f:
+                    string = f.read()
+                    b64d = base64.b64decode(string)
+                    try:
+                        key = PBKDF2(self.hash_key, salt, dkLen=32)
+                        cipher = AES.new(key, AES.MODE_CBC, b64d[:AES.block_size])
+                        d_cipher_data = unpad(cipher.decrypt(b64d[AES.block_size:]), AES.block_size)
+                    except Exception as e:
+                        self.clear()
+                        print(f'Oops..An error has occured. Please try again.\nError: {e}\n\n')
+                        input('Press "Enter" to contine...')
+                        self.clear()
+                with open(file, 'wb') as f2:
+                    f2.write(d_cipher_data)
+                    os.rename(file, file.replace('.oCrypted', ''))
             except Exception as e:
-                self.clear()
-                print(f'Oops..An error has occured. Please try again.\nError: {e}\n\n')
-                input('Press "Enter" to contine...')
-                self.clear()
-                FLAG2 = False
-
-        with open(file, 'wb') as f2:
-            f2.write(d_cipher_data)
-            os.rename(file, file.replace('.oCrypted', ''))
-        
-        if FLAG2 == True:
+                return False
             return True
-        elif FLAG2 == False:
+        else:
             return False
+
+
+
+
+    def dir_encrypt(self, dir_path, salt):
+        isDirectory = os.path.isdir(dir_path)
+        if isDirectory == True:
+            BUF_SIZE = 65536
+            for path, subdirs, files in os.walk(dir_path):
+                for name in files:
+                    try:
+                        self.file_encrypt(f'{path}/{name}', salt)
+                    except Exception as e:
+                        return False
+                return True
+        else:
+            return False
+
+    def dir_decrypt(self, dir_path, salt):
+        isDirectory = os.path.isdir(dir_path)
+        if isDirectory == True:
+            BUF_SIZE = 65536
+            for path, subdirs, files in os.walk(dir_path):
+                for name in files:
+                    try:
+                        self.file_decrypt(f'{path}/{name}', salt)
+                    except Exception as e:
+                        return False
+                return True
+        else:
+            return False
+        
+        
